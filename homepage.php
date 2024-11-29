2e5dcdce6da76714fa7562134ef85942e3772522
IanMurillo2304 <ianbenedictmurillo2304@gmail.com>
2024-11-29 16:42:45 +0800

    rechecking

--- homepage.php
+++ homepage.php
@@ -29,17 +29,17 @@ include("connect.php");
         <form method ="post" action="searchFilterInterface.php">
             <input type="submit" class="btn" value="Edit Item" name="editFitlterButton">
         </form>
-        <form method="post">
-    
+<form method="post"> <!-- Added a button element sis -->
 <div class="input-group">
         <input type="text" class="form-control" id="live_search" autocomplete="off" placeholder="Search Filter Code or Name...">
         <button type="submit" class="search-btn">
-            <i class="fas fa-search"></i>
+        <i class="fas fa-search"></i>
         </button>
     </div>
 </form>
-
 <div id="searchresult"></div>
+
+
 
         <div id="searchresult"></div>
          <!-- Display Filters Table -->
